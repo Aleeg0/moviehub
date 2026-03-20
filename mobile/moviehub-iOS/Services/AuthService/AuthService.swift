2d39059284e473f2e3b95191dd9a1c6d814bae5a
pavel.bychkouski <ipasha1337@yandex.by>
2026-03-21 01:05:24 +0300

    [MOVIE-22]: added auth service

---
+++ mobile/moviehub-iOS/Services/AuthService/AuthService.swift
@@ -0,0 +1,55 @@
+//
+//  AuthService.swift
+//  moviehub-iOS
+//
+//  Created by Pavel Playerz0redd on 19.03.26.
+//
+
+import Foundation
+
+protocol IAuthService {
+    func login(model: LoginDto) async throws
+    func register(model: RegisterDto) async throws
+    func signOut()
+}
+
+final class AuthService: IAuthService {
+    
+    private let networkManager: INetworkManager
+    private let decoder: IDecodeManager
+    private let privateStorage: IPrivateManager
+    
+    private let privateStorageTokenKey = "token"
+    
+    init(networkManager: INetworkManager, decoder: IDecodeManager, privateStorage: IPrivateManager) {
+        self.networkManager = networkManager
+        self.decoder = decoder
+        self.privateStorage = privateStorage
+    }
+    
+    func login(model: LoginDto) async throws {
+        let body: Data? = decoder.encode(data: model)
+        
+        guard let data = try await networkManager.sendRequest(type: .post, url: "path", body: body) else { throw NSError() }
+        
+        let token: String = decoder.decode(data: data)
+        
+        privateStorage.store(key: privateStorageTokenKey, object: token)
+        
+    }
+    
+    func register(model: RegisterDto) async throws {
+        let body: Data? = decoder.encode(data: model)
+        
+        guard let data = try await networkManager.sendRequest(type: .post, url: "path", body: body) else { throw NSError() }
+        
+        let token: String = decoder.decode(data: data)
+        
+        privateStorage.store(key: privateStorageTokenKey, object: token)
+    }
+    
+    func signOut() {
+        privateStorage.remove(key: privateStorageTokenKey)
+    }
+    
+}
