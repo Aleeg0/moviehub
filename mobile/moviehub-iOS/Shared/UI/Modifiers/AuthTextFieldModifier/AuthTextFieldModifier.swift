27b26eb8095061ba6d35f747bd2310537235ba01
pavel.bychkouski <ipasha1337@yandex.by>
2026-03-24 00:13:01 +0300

    [MOVIE-22]: added common modifier for auth text field

--- mobile/moviehub-iOS/Shared/UI/Modifiers/AuthTextFieldModifier/AuthTextFieldModifier.swift
+++ mobile/moviehub-iOS/Shared/UI/Modifiers/AuthTextFieldModifier/AuthTextFieldModifier.swift
@@ -10,13 +10,41 @@ import SwiftUI
 
 struct AuthTextFieldModifier: ViewModifier {
     
+    let error: (AuthValidationError)?
+    
     func body(content: Content) -> some View {
         content
+            .textInputAutocapitalization(.never)
+            .autocorrectionDisabled()
             .font(.system(size: 20, weight: .semibold))
             .frame(maxWidth: .infinity)
             .padding(.horizontal, 20)
             .padding(.vertical, 14)
-            .background(Capsule().foregroundStyle(.pickerGray))
+            .background {
+                Capsule()
+                    .foregroundStyle(.pickerGray)
+                    .overlay {
+                        Capsule()
+                            .stroke(error == nil ? .authBlueDown : .red , lineWidth: 1)
+                    }
+            }
+            .keyframeAnimator(initialValue: 0, trigger: error) { content, value in
+                content
+                    .offset(x: value)
+            } keyframes: { _ in
+                KeyframeTrack {
+                    if let _ = error {
+                        CubicKeyframe(10, duration: 0.1)
+                        CubicKeyframe(-10, duration: 0.1)
+                        CubicKeyframe(5, duration: 0.1)
+                        CubicKeyframe(-5, duration: 0.1)
+                        CubicKeyframe(0, duration: 0.1)
+                    } else {
+                        MoveKeyframe(0)
+                    }
+                }
+            }
+
     }
     
     
