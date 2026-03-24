06e2005b7615b05ed3b02c4ed1da7992958438a6
pavel.bychkouski <ipasha1337@yandex.by>
2026-03-24 19:14:05 +0300

    [MOVIE-21]: added common user model

---
+++ mobile/moviehub-iOS/Domain/Models/UserModel/UserModel.swift
@@ -0,0 +1,14 @@
+//
+//  UserModel.swift
+//  moviehub-iOS
+//
+//  Created by Pavel Playerz0redd on 24.03.26.
+//
+
+import Foundation
+
+
+struct UserModel {
+    let name: String
+    let email: String
+}
